4ecab11a0756f392db711cd1129412b2e82f8184
NaimurRahmannn <niamurreiman@gmail.com>
2026-02-23 12:10:27 +0600

    feat: add login, registration

--- Ecommerce_Storefront/settings.py
+++ Ecommerce_Storefront/settings.py
@@ -121,4 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "public/static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
+MEDIA_URL = '/media/'
