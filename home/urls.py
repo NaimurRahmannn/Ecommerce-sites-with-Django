b32318a3c15f523aab7b2b5b05cee17017345595
NaimurRahmannn <niamurreiman@gmail.com>
2026-03-05 11:59:29 +0600

    feat: add cotact.html

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from home.views import index
+from home.views import index, contact
 urlpatterns = [
-     path('',index, name="index")
+     path('',index, name="index"),
+     path('contact/', contact, name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ from products.models import Product
 def index(request):
     context = {'products' : Product.objects.all()}
     return render(request , 'home/index.html' , context)
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
