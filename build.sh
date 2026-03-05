8099d0da92a49a0765e525cf0e43fe62b4ce83ec
NaimurRahmannn <niamurreiman@gmail.com>
2026-03-06 05:43:12 +0600

    product in render.com

--- build.sh
+++ build.sh
@@ -5,3 +5,13 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin@haatify.com').exists():
+    User.objects.create_superuser('admin@haatify.com', 'admin@haatify.com', 'Haatify@2026')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
