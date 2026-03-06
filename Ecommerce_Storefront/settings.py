3738844d921a8085ac293982c46909f3c910b491
NaimurRahmannn <niamurreiman@gmail.com>
2026-03-06 13:25:38 +0600

    Add LOGGING config for error visibility, fix CSRF origin case

--- Ecommerce_Storefront/settings.py
+++ Ecommerce_Storefront/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://Haatify.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://haatify.onrender.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
@@ -149,4 +149,21 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-SITE_URL = 'https://Haatify.onrender.com'
+SITE_URL = 'https://haatify.onrender.com'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
