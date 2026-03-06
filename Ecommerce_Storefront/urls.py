9af5473a9b4c3da1056cc35d2eeb5fc3b331ccb3
NaimurRahmannn <niamurreiman@gmail.com>
2026-03-06 14:14:26 +0600

    Fix: use re_path to serve media files regardless of DEBUG setting

--- Ecommerce_Storefront/urls.py
+++ Ecommerce_Storefront/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+
 urlpatterns = [
     path('',include('home.urls')),
     path('product/' , include('products.urls') ),
     path('accounts/',include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
 urlpatterns += staticfiles_urlpatterns()
