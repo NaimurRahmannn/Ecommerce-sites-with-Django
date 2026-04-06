9c199bc403332f4c66d729582e861c4bb5b1adf2
NaimurRahmannn <niamurreiman@gmail.com>
2026-04-06 23:53:42 +0600

    fix: set production ALLOWED_HOSTS to Render domain

--- Ecommerce_Storefront/settings.py
+++ Ecommerce_Storefront/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['haatify.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://Haatify.onrender.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
